7cead9c9b5257c91ab6cdddc181a7ed03d5daed4
maciekm88 <maciek.m.88@gmail.com>
2021-12-07 16:25:38 +0100

    Added binary calculator

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 import DecimalCalculator from "./DecimalCalculator";
+import BinaryCalculator from "./BinaryCalculator";
 
 
 document.addEventListener("DOMContentLoaded", () => {
     const decCalc = new DecimalCalculator(".dec-calculator");
+    const bitCalc = new BinaryCalculator(".binary-calculator");
 });
